fbff9cb4291b18ecf829208d2d67ba7cb03dfb7d
shkola <shkola@mail.ru>
2019-12-11 19:00:18 +0500

    practica

--- параграф18/Z12/12/main.cpp
+++ параграф18/Z12/12/main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
-    int x;
-    cin >> x;
-    cout << ((x/8) %2) && 10 << ((x/4) % 2) && 10 << ((x/2) %2) && 10 <<(x %2 )&& 10;
+    int x, a,b,d, e,f, r,c,c1,c2,c3,c4;
+   cout<<a;
+    b = a%10 ; c=b*1;
+     d = a%100 ;c1= d*2;
+     e = a%1000  ;c2= e*4;
+     f = a%10000  ;c3= f*8;
+     r = a%100000 ;c4= r*16;
+     x = c+c1+c2+c3+c4;
+     cin >> x;
     return 0;
 }
 
--- параграф18/Z15/15/main.cpp
+++ параграф18/Z15/15/main.cpp
@@ -6,6 +6,23 @@ int main()
 {
     int x;
     cin >> x;
-    cout << (x/16) % 16 << x % 16;
+
+    int ch1 = x % 16;
+    int ch2 = x / 16;
+
+    if (ch2 < 10) {
+        cout << ch2;
+    } else {
+        cout << char('a' + ch2 - 10);
+    }
+
+
+    if (ch1 < 10) {
+        cout << ch1;
+    } else {
+        cout << char('a' + ch1 - 10);
+    }
+
+    //cout << (x/16) % 16 << x % 16;
     return 0;
 }
