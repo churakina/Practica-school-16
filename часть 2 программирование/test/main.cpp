2b0412f2c602066f8b77e930f0cd2f5535d6af87
shkola <shkola@mail.ru>
2020-01-15 17:35:07 +0500

    practica

---
+++ часть 2 программирование/test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "TXLib.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    txCreateWindow(500, 600);
+    return 0;
+}
---
+++ часть 2 программирование/параграф 12/задание/1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "TXLib.h"
+
+using namespace std;
+    void pythagorasTree (float x, float y, float L, float angle, int levels)
+    {
+        if (levels <=0) return;
+        const float k = 0.7;
+        const float angle1 = angle - 45;
+        const float angle2 = angle + 45;
+        float x1 = x + L*cos (angle*M_PI/180);
+        float y1 = y - L*sin (angle*M_PI/180);
+        txLine (x, y, x1, y1);
+        pythagorasTree (x1, y1, k*L, angle1, levels-1);
+        pythagorasTree (x1, y1, k*L, angle2, levels-1);
+
+    }
+    int main ()
+    {
+    txCreateWindow (800, 600);
+    pythagorasTree (100, 400, 150, 90, 5 );
+    }
